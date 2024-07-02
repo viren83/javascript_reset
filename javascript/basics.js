f760f899ea5b6468cbeea5aa9a92b4ecc94127e2
viren83 <78212356+viren83@users.noreply.github.com>
2024-07-02 09:34:50 +0000

    tailwindcss

---
+++ javascript/basics.js
@@ -0,0 +1,15 @@
+console.log("Hello");
+
+// Example of each data type
+const numberExample = 10;
+const stringExample = "Hello, World!";
+const booleanExample = true;
+const arrayExample = [1, 2, 3];
+const objectExample = { name: "John", age: 30 };
+
+console.log(numberExample);
+console.log(stringExample);
+console.log(booleanExample);
+console.log(arrayExample);
+console.log(objectExample);
+
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./javascript/**/*.js", "./layouts/**/*.html, ./dist/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
